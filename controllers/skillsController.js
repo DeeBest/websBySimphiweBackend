42eea48d80dddb7dea796a23d6ef146fe899579c
DeeBest <simphiwedladla8@gmail.com>
2025-02-11 19:50:37 +0200

    finished CRUD endpoints for the skills api resource

--- controllers/skillsController.js
+++ controllers/skillsController.js
@@ -13,6 +13,22 @@ const getAllSkills = async (req, res) => {
   }
 };
 
+const getSingleSkill = async (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(400).json({ message: 'No ID was provided' });
+  try {
+    const skill = await Skill.findOne({ _id: id }).exec();
+    if (!skill)
+      return res
+        .status(404)
+        .json({ message: `No skill with ID ${id} was found.` });
+    res.status(200).json({ message: 'Success', skill: skill });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const addSkill = async (req, res) => {
   const { skillName, skillDesc, skillImgUrl, category } = req.body;
 
@@ -39,4 +55,56 @@ const addSkill = async (req, res) => {
   }
 };
 
-module.exports = { getAllSkills, addSkill };
+const updateSkill = async (req, res) => {
+  const id = req.params.id;
+  if (!id) return res.status(400).json({ message: 'No ID was provided' });
+
+  try {
+    const skill = await Skill.findOne({ _id: id });
+    if (!skill)
+      return res
+        .status(404)
+        .json({ message: `No skill with ID ${id} was found.` });
+
+    if (req?.body?.skillName) skill.skillName = req.body.skillName;
+    if (req?.body?.skillDesc) skill.skillDesc = req.body.skillDesc;
+    if (req?.body?.skillImgUrl) skill.skillImgUrl = req.body.skillImgUrl;
+    if (req?.body?.category) skill.category = req.body.category;
+
+    await skill.save();
+
+    res.status(200).json({ message: 'Successfully updated the skill', skill });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteSkill = async (req, res) => {
+  const { id } = req.params;
+  if (!id)
+    return res.status(400).json({ message: 'Please provide a valid ID' });
+
+  try {
+    const skill = await Skill.findOne({ _id: id });
+    if (!skill)
+      return res
+        .status(404)
+        .json({ message: `No skill with ${id} ID was found.` });
+    await Skill.deleteOne(skill);
+    res
+      .status(204)
+      .json({ message: `Successfully deleted ${skill.skillName} skill.` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllSkills,
+  addSkill,
+  getSingleSkill,
+  updateSkill,
+  deleteSkill,
+};
--- routes/skillsRoutes.js
+++ routes/skillsRoutes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
-const { getAllSkills, addSkill } = require('../controllers/skillsController');
+const {
+  getAllSkills,
+  addSkill,
+  getSingleSkill,
+  updateSkill,
+  deleteSkill,
+} = require('../controllers/skillsController');
 
 router.get('/', getAllSkills);
+router.get('/:id', getSingleSkill);
 router.post('/add-skill', addSkill);
+router.put('/update-skill/:id', updateSkill);
+router.delete('/delete-skill/:id', deleteSkill);
 
 module.exports = router;
