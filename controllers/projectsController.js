89cfa61ea6e7f2f5e1cce4e22e05ca57fa2c156c
DeeBest <simphiwedladla8@gmail.com>
2025-02-11 06:05:45 +0200

    Finished CRUD endpoints for the projects resource

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -12,6 +12,25 @@ const getAllProjects = async (req, res) => {
   }
 };
 
+const getSingleProject = async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(400).json({ message: 'ID is required' });
+  }
+  try {
+    const project = await Project.findOne({ _id: id }).exec();
+    if (!project) {
+      return res
+        .status(404)
+        .json({ message: `No project with id ${id} exists.` });
+    }
+    res.status(200).json({ message: 'Success', project: project });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const addProject = async (req, res) => {
   const {
     projectName,
@@ -62,6 +81,37 @@ const addProject = async (req, res) => {
   }
 };
 
+const updateProject = async (req, res) => {
+  const id = req.params.id;
+  if (!id) return res.status(400).json({ message: 'Please provide ID' });
+  try {
+    const project = await Project.findOne({ _id: id });
+    if (!project)
+      return res
+        .status(404)
+        .json({ message: `No project with ${id} ID exists.` });
+
+    if (req?.body?.projectName) project.projectName = req.body.projectName;
+    if (req?.body?.projectDesc) project.projectDesc = req.body.projectDesc;
+    if (req?.body?.codeReviewLink)
+      project.codeReviewLink = req.body.codeReviewLink;
+    if (req?.body?.livePreviewLink)
+      project.livePreviewLink = req.body.livePreviewLink;
+    if (req?.body?.imgUrl) project.imgUrl = req.body.imgUrl;
+    if (req?.body?.category) project.category = req.body.category;
+    if (req?.body?.usedTechStack)
+      project.usedTechStack = req.body.usedTechStack;
+
+    await project.save();
+    res
+      .status(200)
+      .json({ message: `Successfully updated project`, project: project });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const deleteProject = async (req, res) => {
   const id = req.params.id;
 
@@ -83,4 +133,10 @@ const deleteProject = async (req, res) => {
   }
 };
 
-module.exports = { getAllProjects, addProject, deleteProject };
+module.exports = {
+  getAllProjects,
+  addProject,
+  deleteProject,
+  getSingleProject,
+  updateProject,
+};
--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -5,10 +5,14 @@ const {
   getAllProjects,
   addProject,
   deleteProject,
+  getSingleProject,
+  updateProject,
 } = require('../controllers/projectsController');
 
 router.get('/', getAllProjects);
 router.post('/add-project', addProject);
 router.delete('/:id', deleteProject);
+router.get('/:id', getSingleProject);
+router.put('/update-project/:id', updateProject);
 
 module.exports = router;
