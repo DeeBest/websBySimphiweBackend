bad1819c006806790a22b34ebeb5f9762659bf43
DeeBest <simphiwedladla8@gmail.com>
2025-03-13 12:22:25 +0200

    configured the backend to use aws s3 for image storage

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,27 +1,34 @@
 const multer = require('multer');
-const path = require('path');
+const multerS3 = require('multer-s3');
+const { S3Client } = require('@aws-sdk/client-s3');
 
-// Creating upload storage
-const storage = multer.diskStorage({
-  destination: './uploads/images',
-  filename: (req, file, cb) => {
-    if (!file.originalname.match(/\.(png|svg|jpg|jpeg|ico)$/)) {
-      return cb(new Error('Please provide correct file type.'));
-    }
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-    );
+const s3 = new S3Client({
+  region: process.env.AWS_REGION, // e.g., 'us-east-1'
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE, // Automatically set the content type based on the file
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, `${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
 
 // Controller for handling image uploads
 exports.uploadImage = (req, res) => {
   res.json({
     message: 'Success',
-    imageUrl: `${process.env.API_URL}/images/${req.file.filename}`,
+    imageUrl: req.file.location, // S3 URL of the uploaded image
   });
 };
 
