6b7393029ff8b988ebd150a3595d3bc51d245182
DeeBest <simphiwedladla8@gmail.com>
2025-03-17 14:19:34 +0200

    added the frontend deployed app address to the allowed origins config file

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
+  'https://websbysimphiwe.onrender.com',
 ];
 
 module.exports = allowedOrigins;
